8f0f3e9aa250749f61614898aec3aa7c3b76349a
North <NorthRiver@laposte.net>
2017-05-18 14:57:55 +0200

    Monitoring Server

--- monitoring/src/main.c
+++ monitoring/src/main.c
@@ -1,5 +1,100 @@
 #include "main.h"
 
-int main(int argc,char* args[]) {
-	
+
+char adServeur [15] = "localhost"; // A completer avec votre ip
+
+void proc_exit(int sig) {
+  wait();
+}
+
+char * readFile(char * fileName){
+	FILE * file;
+	file = fopen( fileName , "r");
+	char[12] res;
+	fscanf(file, "%s", res);
+	return res;
+
+}
+
+
+int main(void) {
+
+  /* Pour eviter les zombies */
+  signal(SIGCHLD, proc_exit);
+
+  /* Socket et contexte d'adressage du serveur */
+  struct sockaddr_in sin;
+  int sock;
+  socklen_t recsize = sizeof(sin);
+    
+  /* Socket et contexte d'adressage du client */
+  struct sockaddr_in csin;
+  int csock;
+  socklen_t crecsize = sizeof(csin);
+
+  /* Pour forker le moment venu */
+  pid_t pid;
+ 
+  /* Creation d'une socket */
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+
+  /* Configuration */
+  sin.sin_addr.s_addr = htonl(INADDR_ANY);  /* Adresse IP automatique */
+  sin.sin_family = AF_INET;                 /* Protocole familial (IP) */
+  sin.sin_port = htons(PORT);               /* Listage du port */
+  bind(sock, (struct sockaddr*)&sin, recsize);
+
+  /* Demarrage du listage (mode server) */
+  listen(sock, 5);
+
+  for(;;) {
+    /* Attente d'une connexion client */
+    csock = accept(sock, (struct sockaddr*)&csin, &crecsize);
+    printf("Demande du serveur de monitoring reçu.\n");
+
+    pid = fork();
+
+    if (pid == 0) {
+      /* Envoi de donnees au client */
+    	char[12] pos;
+    	char[12] verfiedPos;
+
+    	pos = readFile("../dataPosition/position.txt");
+    	verfiedPos = readFile("../dataPosition/verifiedPosition.txt");
+
+      	char buffer[50] = "";
+		strcat(buffer,adServeur);
+		strcat(buffer,"/");
+
+		strcat(buffer,pos);
+
+		strcat(buffer,"/");
+
+		strcat(buffer,verfiedPos);
+
+		send(csock, buffer,50 , 0);
+		printf("Envoi de %s\n", buffer);
+
+      for(;;) {
+        /* Reception de donnees du client */
+        int res = recv(csock, buffer, 32, 0);
+        if (res == 0){
+          exit(0);
+        }
+        printf("Recu de la socket %d de %s:%d : %s\n", csock, inet_ntoa(csin.sin_addr), htons(csin.sin_port), buffer);
+      }
+
+      /* Fermeture de la socket dans les deux sens */
+      shutdown(csock, 2);
+
+      /* Fermeture de la socket client */
+      close(csock);
+    }//if
+  }//for
+
+  /* Fermeture de la socket serveur */
+  close(sock);
+
+  return EXIT_SUCCESS;
+
 }
--- monitoring/src/main.h
+++ monitoring/src/main.h
@@ -1,2 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#define PORT 10000
 
 int main(int argc,char* args[]);
+
+/** Retourne la chaine de caractere lu dans le fichier passée en parametre
+* @param fileName La nom du fichier a lire
+* @return la chaine lu
+*/ 
+char * readFile(char * fileName);
