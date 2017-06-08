58349a14b554ffb372ac19cfa7004e1e0838e542
North <NorthRiver@laposte.net>
2017-06-08 17:15:21 +0200

    qsdf

--- src/socket.c
+++ src/socket.c
@@ -16,30 +16,31 @@
 
 
 char * socketRecupRobot(char * ip) {
-	int sock;
-	
+    int sock;
+
     // COMMUNICATION SOCKET
     struct sockaddr_in sin;
 
     /* Creation de la socket */
     sock = socket(AF_INET, SOCK_STREAM, 0);
-   
+
     /* Configuration de la connexion */
+    printf("%s", ip)
     sin.sin_addr.s_addr = inet_addr(ip);
     sin.sin_family = AF_INET;
     sin.sin_port = htons(PORT);
-   	printf("Tentative de connexion\n");
+    printf("Tentative de connexion\n");
     /* Tentative de connexion au serveur */
     connect(sock, (struct sockaddr*)&sin, sizeof(sin));
-    printf("\nConnexion a %s sur le port %d\n", inet_ntoa(sin.sin_addr),htons(sin.sin_port));
-     printf("Get informations du robot\n");
+    printf("\nConnexion a %s sur le port %d\n", inet_ntoa(sin.sin_addr), htons(sin.sin_port));
+    printf("Get informations du robot\n");
 
 
     /* Reception de donnees du serveur */
-    char buffer[50] = "";
+    char buffer [50] = "";
     recv(sock, buffer, 32, 0);
     printf("Reçu : %s\n", buffer);
-    char * res = malloc(50*sizeof(char));
+    char * res = malloc(50 * sizeof(char));
     strcpy(res, buffer);
     return res;
 }
