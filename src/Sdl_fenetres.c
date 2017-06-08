489a1b65fbf2747962500aebbf246314ef8955ee
North <NorthRiver@laposte.net>
2017-06-08 17:33:36 +0200

    JHFG

--- src/Sdl_fenetres.c
+++ src/Sdl_fenetres.c
@@ -131,6 +131,7 @@ int partie(){
     setBallon(&r, 0);
     printf("\n[\033[33mDEBUG\033[0m] Test b\n");
     setIp(&r, tok);
+    printf("%s", getIp(&r));
     printf("\n[\033[33mDEBUG\033[0m] Test b2\n");
     pushRobotRouge(laPartie, &r);
     printf("\n[\033[33mDEBUG\033[0m] Test b3\n");
