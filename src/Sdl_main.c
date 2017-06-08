7e73115dbbe369cca0ed5c96001befdcb038dd07
North <NorthRiver@laposte.net>
2017-06-08 12:24:00 +0200

    TEst

--- src/Sdl_main.c
+++ src/Sdl_main.c
@@ -6,15 +6,15 @@ int main(int argc, char* args[]) {
     init();
 
 
-
-    while (menuSelect != 0) {
-        if (menuSelect == 1) {
-            menuSelect = loading();
-        }
-        else if (menuSelect == 2) {
-            menuSelect = partie();
-        }
-    }
+    //
+    // while (menuSelect != 0) {
+    //     if (menuSelect == 1) {
+    //         menuSelect = loading();
+    //     }
+    //     else if (menuSelect == 2) {
+    //         menuSelect = partie();
+    //     }
+    // }
 
     cleanup();
 
